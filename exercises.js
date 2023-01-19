b4be436c45110c4cf03c1af1bf74b73ac27ea901
Noel Fernandez <noelfernandez98@gmail.com>
2023-01-19 03:37:32 -0500

    testing .1 complete

--- exercises.js
+++ exercises.js
@@ -1,36 +1,83 @@
 // Question 1
-const isUpperCase = () => {
-
+const isUpperCase = (str) => {
+  return str === str.toUpperCase();
 };
 
 // Question 2
-const removeVowels = () => {
-
+const removeVowels = (arr) => {
+  for (let i = 0; i < arr.length; i++){
+    arr[i] = arr[i].replace(/[a,e,i,o,u]/gi, "")
+  }
+  return arr;
 };
 
 // Question 3
-const wordCap = () => {
-
+const wordCap = (str) => {
+  str = str.toLowerCase().split(" ")
+  for (let i = 0; i < str.length; i++){
+    str[i]= str[i][0].toUpperCase() + str[i].substring(1);
+  }
+  return str.join(" ")
 };
 
 // Question 4
-const swapCase = () => {
-
+const swapCase = (str) => {
+  let newStr =""
+  for (let i = 0; i < str.length; i++){
+    if (str[i] === str[i].toLowerCase()) {
+      newStr += str[i].toUpperCase()
+    } else if (str[i] === str[i].toUpperCase()) {
+      newStr += str[i].toLowerCase()
+    }
+  }
+  return newStr;
 };
 
 // Question 5
-const staggeredCase = () => {
-
+const staggeredCase = (str) => {
+  let newStr = "";
+  let toggle = true;
+  for (let i = 0; i < str.length; i++){
+    if ((/[a-z]/i).test(str[i])) {
+      if (toggle) {
+        newStr += str[i].toUpperCase();
+      } else {
+        newStr += str[i].toLowerCase();
+      }
+      toggle = !toggle
+    } else {
+      newStr += str[i];
+    }
+  }
+  return newStr;
 };
 
 // Question 6
-const wordLengths = () => {
-
+const wordLengths = (str) => {
+  let arr = [];
+  if (str == "" || !str) {
+    return arr
+  }
+  str = str.split(" ")
+  for (let i = 0; i < str.length; i++){
+    str[i].replace(/\W/g, "");
+    arr.push(`${str[i]} ${str[i].length}`)
+  }
+  return arr
 };
 
 // Question 7
-const searchWord = () => {
-
+const searchWord = (str, text) => {
+  counter = 0;
+  text = text.split(" ")
+  console.log(text);
+  for (let i = 0; i < text.length; i++){
+    text[i] = text[i].replace(/,/g, "");
+    if (text[i].toLowerCase() === str) {
+      counter ++
+    }
+  }
+  return counter;
 };
 
 
--- exercises.test.js
+++ exercises.test.js
@@ -8,3 +8,51 @@ test('Question 0: add', () => {
   expect(exercises.add(1, -1)).toEqual(0);
 });
 */
+
+test('Question 1: isUpperCase', () => {
+  expect(exercises.isUpperCase('t')).toBe(false);
+  expect(exercises.isUpperCase('T')).toBe(true);
+  expect(exercises.isUpperCase('Four Score')).toBe(false);
+  expect(exercises.isUpperCase('FOUR SCORE')).toBe(true);
+  expect(exercises.isUpperCase('4SCORE!')).toBe(true);
+  expect(exercises.isUpperCase('')).toBe(true);
+});
+
+test('Question 2: removeVowels', () => {
+  expect(exercises.removeVowels(['abcdefghijklmnopqrstuvwxyz'])).toEqual(["bcdfghjklmnpqrstvwxyz"]);
+  expect(exercises.removeVowels(['green', 'YELLOW', 'black', 'white'])).toEqual(["grn", "YLLW", "blck", "wht"]);
+  expect(exercises.removeVowels(['ABC', 'AEIOU', 'XYZ'])).toEqual(["BC", "", "XYZ"]);
+});
+
+test('Question 3: wordCap', () => {
+  expect(exercises.wordCap('four score and seven')).toBe("Four Score And Seven");
+  expect(exercises.wordCap('the javaScript language')).toBe("The Javascript Language");
+  expect(exercises.wordCap('this is a "quoted" word')).toBe('This Is A "quoted" Word');
+});
+
+test('Question 4: swapCase', () => {
+  expect(exercises.swapCase('CamelCase')).toBe("cAMELcASE");
+  expect(exercises.swapCase('Tonight on XYZ-TV')).toBe("tONIGHT ON xyz-tv");
+});
+
+test('Question 5: staggeredCase', () => {
+  expect(exercises.staggeredCase('I Love Launch School!')).toBe("I lOvE lAuNcH sChOoL!");
+  expect(exercises.staggeredCase('ALL CAPS')).toBe("AlL cApS");
+  expect(exercises.staggeredCase('ignore 77 the 444 numbers')).toBe("IgNoRe 77 ThE 444 nUmBeRs");
+});
+
+test('Question 6: wordLengths', () => {
+  expect(exercises.wordLengths('cow sheep chicken')).toEqual(["cow 3", "sheep 5", "chicken 7"]);
+  expect(exercises.wordLengths('baseball hot dogs and apple pie')).toEqual(["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]);
+  expect(exercises.wordLengths("It ain't easy, is it?")).toEqual(["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]);
+  expect(exercises.wordLengths('Supercalifragilisticexpialidocious')).toEqual(["Supercalifragilisticexpialidocious 34"]);
+  expect(exercises.wordLengths('')).toEqual([]);
+  expect(exercises.wordLengths()).toEqual([]);
+});
+
+test('Question 7: searchWord', () => {
+  let text = 'D Smoke is humble. The Inglewood native exudes an aura of maturation, needed for his quick ascension into popular culture as the first winner of Rhythm + Flow, Netflix’s hip-hop reality competition centered on the discovery of hip-hop’s next star. His signature authenticity shone throughout the 10-episode series and international audiences were drawn to his charisma as he proudly rapped about his lived experiences as a young black man in Inglewood.';
+  expect(exercises.searchWord('his', text)).toBe(4);
+  expect(exercises.searchWord('hip-hop', text)).toBe(1); // "hip-hop" SHOULD not match "hip-hop's"
+  expect(exercises.searchWord('flow', text)).toBe(1);
+});
